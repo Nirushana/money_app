56f010bc8c899b3e829645a7ef7474661eca0df7
Nirushana <kavinda.n.t@gmail.com>
2022-05-09 00:23:07 +0400

    new commit

--- Screens/Welcome.tsx
+++ Screens/Welcome.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
 import { Container } from "../Components/shared";
 import { colors } from "../Components/colors";
+import background from "../assets/bgs/background_v1.png"
 
 const WelcomeContainer = styled(Container)`
  background-color: ${colors.secondary};
@@ -10,13 +11,31 @@ const WelcomeContainer = styled(Container)`
  width: 100%;
  height: 100%;
 `
+const TopSection =  styled.View`
+  width: 100%;
+  flex: 1;
+  max-height: 55%;
+`
+const TopImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`
+const BottomSection = styled.View`
+  width: 100%;
+  padding: 25px;
+  flex: 1;
+`
+
 
 const Welcome: FunctionComponent = () => {
   return (
     <>
       <StatusBar style="light" />
       <WelcomeContainer>
-          
+          <TopSection>
+            <TopImage source={background} />
+          </TopSection>
+          <BottomSection></BottomSection>
       </WelcomeContainer>
     </>
   );
