b3d79dc0319dc6619978a446fa7379ba94e44a2b
Nirushana <kavinda.n.t@gmail.com>
2022-05-11 19:25:49 +0400

    Commited

---
+++ navigators/RookStack.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+//React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const RootStack: FunctionComponent = () => {
+    return<></>
+} 
