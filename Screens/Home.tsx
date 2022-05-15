a8bab6c2feac2b3eaa739f08c0b6be1363896332
Nirushana <kavinda.n.t@gmail.com>
2022-05-15 22:46:06 +0400

    Commited

---
+++ Screens/Home.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React, { FunctionComponent } from 'react'
+import styled from 'styled-components/native';
+import { Container } from '../Components/shared';
+import { colors } from "../Components/colors";
+
+const HomeContainer = styled(Container)`
+    background-color: ${colors.graylight};
+    width: 100%;
+    flex: 1;
+`
+
+const Home: FunctionComponent = () => {
+    return(
+        <HomeContainer>
+            <StatusBar style="dark" />
+        </HomeContainer>
+    );
+}
+
+export default Home;
--- navigators/RookStack.tsx
+++ navigators/RookStack.tsx
@@ -3,12 +3,15 @@ import React, { FunctionComponent } from 'react';
 //React Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { colors } from '../Components/colors';
 
 //Screen
-import Welcome from '../Screens/Welcome'
+import Welcome from '../Screens/Welcome';
+import Home from '../Screens/Home';
 
 export type RootStackParamList = {
     Welcome: undefined;
+    Home: undefined;
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -16,11 +19,15 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const RootStack: FunctionComponent = () => {
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName='Home'>
                 <Stack.Screen
                     name='Welcome'
                     component={Welcome}
                     options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='Home'
+                    component={Home}
                 />
             </Stack.Navigator>
         </NavigationContainer>
