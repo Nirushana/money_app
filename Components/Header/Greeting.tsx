cb14dd3e80778e2785ab4ec33439fb6e15df7f63
Nirushana <kavinda.n.t@gmail.com>
2022-05-16 16:06:11 +0400

    Home HeaderSection Greetings

--- Components/Header/Greeting.tsx
+++ Components/Header/Greeting.tsx
@@ -1,18 +1,41 @@
-import React, { FunctionComponent } from 'react';
-import styled from 'styled-components/native';
-import { StyleProp, TextStyle } from 'react-native'
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { StyleProp, TextStyle } from "react-native";
+
+//Custom Components
+import RegularText from "../Text/RegularText";
+import SmallText from "../Text/SmallText";
+import { colors } from "../colors";
+
+const StyledView = styled.View`
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+`;
 
 interface GreetingProps {
-    mainText: string;
-    subText: string;
-    mainTextStyles?: StyleProp<TextStyle>;
+  mainText: string;
+  subText: string;
+  mainTextStyles?: StyleProp<TextStyle>;
+  subTextStyles?: StyleProp<TextStyle>;
 }
 
+const Greeting: FunctionComponent<GreetingProps> = (props) => {
+  return (
+    <StyledView>
+      <RegularText
+        textStyles={[
+          { color: colors.secondary, fontSize: 22 },
+          props.mainTextStyles,
+        ]}
+      >
+        {props.mainText}
+      </RegularText>
+      <SmallText textStyles={[{ color: colors.graydrak }, props.subTextStyles]}>
+        {props.subText}
+      </SmallText>
+    </StyledView>
+  );
+};
 
-const Greeting: FunctionComponent = () =>{
-    return(
-
-    )
-}
-
-export default Greeting
+export default Greeting;
