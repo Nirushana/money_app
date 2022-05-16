ebd25ce7e563c86f818755ee69716b69bdb7639d
Nirushana <kavinda.n.t@gmail.com>
2022-05-16 15:50:36 +0400

    HomePage HeaderScetion

---
+++ Components/Header/Greeting.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components/native';
+import { StyleProp, TextStyle } from 'react-native'
+
+interface GreetingProps {
+    mainText: string;
+    subText: string;
+    mainTextStyles?: StyleProp<TextStyle>;
+}
+
+
+const Greeting: FunctionComponent = () =>{
+    return(
+
+    )
+}
+
+export default Greeting
--- navigators/RookStack.tsx
+++ navigators/RookStack.tsx
@@ -1,37 +1,40 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 
 //React Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { colors } from '../Components/colors';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { colors } from "../Components/colors";
 
 //Screen
-import Welcome from '../Screens/Welcome';
-import Home from '../Screens/Home';
+import Welcome from "../Screens/Welcome";
+import Home from "../Screens/Home";
 
 export type RootStackParamList = {
-    Welcome: undefined;
-    Home: undefined;
-}
+  Welcome: undefined;
+  Home: undefined;
+};
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootStack: FunctionComponent = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen
-                    name='Welcome'
-                    component={Welcome}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name='Home'
-                    component={Home}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-} 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: colors.graylight },
+          headerTintColor: colors.secondary,
+        }}
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default RootStack;
