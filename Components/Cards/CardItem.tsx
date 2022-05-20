f02b5c494c132f2931490a34089dc31cb75da60a
Nirushana <kavinda.n.t@gmail.com>
2022-05-20 23:53:15 +0400

    CardSection FlatList

---
+++ Components/Cards/CardItem.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components/native';
+import { CardProps } from './types';
+
+const CardItem: FunctionComponent<CardProps> = () => {
+    return()
+}
+
+export default CardItem;
