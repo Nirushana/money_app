9ae93bf7058c87d6985bd272fd5f9ad68e8bfecf
Nirushana <kavinda.n.t@gmail.com>
2022-05-21 16:31:19 +0400

    CardSection FlatList

--- Components/Cards/CardItem.tsx
+++ Components/Cards/CardItem.tsx
@@ -1,9 +1,30 @@
-import React, { FunctionComponent } from 'react';
-import styled from 'styled-components/native';
-import { CardProps } from './types';
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { CardProps } from "./types";
+import { ScreenWidth } from "../shared";
+import { colors } from "../colors";
 
-const CardItem: FunctionComponent<CardProps> = () => {
-    return()
-}
+const CardBackground = styled.ImageBackground`
+  height: 75%;
+  width: ${ScreenWidth * 0.67}px;
+  background-color: ${colors.accent};
+  border-radius: 25px;
+  margin-right: 25px;
+  overflow: hidden;
+`;
+const CardTouchable = styled.TouchableHighlight`
+  height: 100%;
+  border-radius: 25px;
+`;
+const TouchableView = styled.View`
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+  flex: 1;
+`;
+
+const CardItem: FunctionComponent<CardProps> = (props) => {
+  return <TouchableView></TouchableView>;
+};
 
 export default CardItem;
--- Components/Cards/CardSection.tsx
+++ Components/Cards/CardSection.tsx
@@ -1,29 +1,29 @@
-import React, { FunctionComponent } from 'react';
-import styled from 'styled-components/native';
-import { CardSectionProps } from './types'
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { CardSectionProps } from "./types";
 
 //components
 const CardList = styled.FlatList`
-    width: 100%;
-    flex: 1;
-    padding-left: 25px;
-    padding-bottom: 15px;
-`
+  width: 100%;
+  flex: 1;
+  padding-left: 25px;
+  padding-bottom: 15px;
+`;
 
 const CardSection: FunctionComponent<CardSectionProps> = (props) => {
-    return(
-        <CardList
-            data={props.data}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-                paddingRight: 25,
-                alignItems: 'center',
-            }}
-            keyExtractor={({id}: any) => id.toString()}
-            renderItem={}
-        />
-    )
-}
+  return (
+    <CardList
+      data={props.data}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingRight: 25,
+        alignItems: "center",
+      }}
+      keyExtractor={({ id }: any) => id.toString()}
+      renderItem={}
+    />
+  );
+};
 
 export default CardSection;
